93e17fb9c76eb94b3db02aa792925a1d1278666d
MIKE <MIKE@LAPTOP-VL7VFGHU>
2025-10-08 11:46:23 +0800

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem_AponteCatiban.Models
+{
+    internal class Customer
+    {
+        public string customerID {  get; set; }
+        public string customerName { get; set; }
+        public DateTime birthdate { get; set; }
+        public string customerContactInfo { get; set; }
+        public string customerEmailAddress { get; set; }
+        public string customerAccountNumber { get; set; }
+        public decimal customerBalance { get; set; }
+
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem_AponteCatiban.Models
+{
+    internal class Transaction
+    {
+        public string customerAccountNumber { get; set; }
+        public DateTime date {  get; set; }
+        public string type { get; set; }
+        public decimal amount { get; set; }
+        public decimal previousBalance { get; set; }
+        public decimal newBalance { get; set; }
+
+    }
+}
